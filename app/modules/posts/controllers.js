87535cc74898cb80f08e4ced85e8f8612b80a27e
Vero Chan <crazyveronik@gmail.com>
2016-07-15 10:25:00 +0200

    - Added view single post functionality and view - Added User service to retrieve data of the creator of the post - Added html5Mode on config, forgot to add it when creating the function

--- app/modules/posts/controllers.js
+++ app/modules/posts/controllers.js
@@ -3,14 +3,40 @@
 	'use strict';
 
 	angular
-		.module('posts.controllers', ['posts.services']);
+		.module('posts.controllers', ['posts.services', 'users.services']);
 
 	function PostListController(Post)
 	{
 		this.posts= Post.query();
 	}
 
+	function PostDetailController($routeParams, Post, User)
+	{
+		this.post={};
+		//this.comment={};
+		this.user={};
+
+		var self=this;
+
+		Post.query({ id: $routeParams.postId})
+			.$promise.then(
+				function(data)
+				{
+					self.post=data[0];
+					self.user=User.query({ id: self.post.userId});
+				},
+				function(error)
+				{
+					console.log(error);
+				}
+			);
+
+		//this.comments=Comment.query({ postId: $routeParams.postId });	
+
+	}
+
 	  angular
 		  .module('posts.controllers')
-		  .controller('PostListController', PostListController);
+		  .controller('PostListController', PostListController)
+		  .controller('PostDetailController', PostDetailController);
 })();
--- app/modules/posts/module.js
+++ app/modules/posts/module.js
@@ -2,10 +2,14 @@
 {
 	'use strict';
 
-	function config ($routeProvider) {  
+	function config ($locationProvider, $routeProvider) {  
+	  $locationProvider.html5Mode(true);
 	  $routeProvider
 	    .when('/', {
 	      template: '<my-posts></my-posts>'
+	    })
+	    .when('/post/:postId', {
+	      template: '<my-post-detail></my-post-detail>'
 	    });
 	}
 
@@ -14,6 +18,11 @@
 			    controller: 'PostListController',
 			    controllerAs: 'postlist',
 			    templateUrl: 'modules/posts/views/post-list.tpl.html'
+			  })
+			.component('myPostDetail', {
+			    controller: 'PostDetailController',
+			    controllerAs: 'postdetail',
+			    templateUrl: 'modules/posts/views/post-detail.tpl.html'
 			  })
 			.config(config);  
 
