b6472dbee53a176be33de03a6968ebce5956ffe0
Vero Chan <crazyveronik@gmail.com>
2016-08-10 12:14:17 +0200

    - Users and Comments from the post changed to stateless components.

--- app/modules/posts/module.js
+++ app/modules/posts/module.js
@@ -6,28 +6,42 @@
 	  $locationProvider.html5Mode(true);
 	  $routeProvider
 	    .when('/', {
-	      template: '<my-posts></my-posts>'
+	      template: ['<posts-list></posts-list']
 	    })
 	    .when('/post/:postId', {
-	      template: '<my-post-detail></my-post-detail>'
+	      template: '<post-detail></post-detail>'
 	    })
 	    .when('/new', {
-	    	template: '<my-new-post></my-new-post>'
+	    	template: '<new-post></new-post>'
 	    });
 	}
 
 	angular.module('posts', ['ngRoute', 'posts.controllers'])
-			.component('myPosts', {
+			.component('postsList', {
 			    controller: 'PostListController',
 			    controllerAs: 'postlist',
 			    templateUrl: 'modules/posts/views/post-list.tpl.html'
 			  })
-			.component('myPostDetail', {
+			.component('postDetail', {
 			    controller: 'PostDetailController',
 			    controllerAs: 'postdetail',
 			    templateUrl: 'modules/posts/views/post-detail.tpl.html'
 			  })
-			.component('myNewPost', {
+			.component('postDetailComments', {
+        		bindings: {
+    				data: '<',
+  				},
+  				controllerAs: 'postcomments', //I could erase controllerAs, and simply use $ctrl, but I prefer this
+			    templateUrl: 'modules/posts/views/post-detail-comments.tpl.html'
+			  })
+			.component('postDetailUser', {
+        		bindings: {
+    				data: '<',
+  				},
+  				controllerAs: 'postuser', //I could erase controllerAs, and simply use $ctrl, but I prefer this
+			    templateUrl: 'modules/posts/views/post-detail-user.tpl.html'
+			  })
+			.component('newPost', {
 			    controller: 'PostCreateController',
 			    controllerAs: 'postcreate',
 			    templateUrl: 'modules/posts/views/post-create.tpl.html'
