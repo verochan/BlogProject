ba2cacabdd7dd18de33672d114f63cc19d8bed1b
Vero Chan <crazyveronik@gmail.com>
2016-08-13 11:52:04 +0200

    - Applying styles to the Users list - Applied responsive design to the post page - Added paginator to posts list

--- app/modules/posts/controllers.js
+++ app/modules/posts/controllers.js
@@ -3,11 +3,19 @@
 	'use strict';
 
 	angular
-		.module('posts.controllers', ['posts.services', 'users.services', 'comments.services']);
+		.module('posts.controllers', ['posts.services', 'users.services', 'comments.services', 'angularSimplePagination']);
 
 	function PostListController(Post)
 	{
 		this.posts={};
+		this.length=0;
+		//PAGINATOR
+		this.settings = {
+				currentPage: 0,
+				offset: 0,
+				pageLimit: 5,
+				pageLimits: ['10', '50', '100']
+				};
 		var self=this;
 
 		Post.query()
@@ -15,12 +23,14 @@
 				function(data)
 				{
 					self.posts=data;
+					self.length=self.posts.length;
 				},
 				function(error)
 				{
 					console.log(error);
 				}
 			);
+		
 	}
 
 	function PostDetailController($routeParams, Post, User, Comment)
